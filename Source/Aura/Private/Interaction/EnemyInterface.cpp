e23f0b1bcc11a295ea27e529f553c37213c57512
MistaMiley <obsMexGames@outlook.com>
2025-09-17 16:36:32 +0100

    started sketching out the interface for  highlighting Actors functionality

---
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright notice for ObsMexGames .2025
+
+
+#include "Interaction/EnemyInterface.h"
+
+// Add default functionality here for any IEnemyInterface functions that are not pure virtual.
