0b46a7b224e23e5161d0f06443c54d5fd6aa34d9
MistaMiley <obsMexGames@outlook.com>
2025-10-02 22:51:37 +0100

    fawkin finally on 31 HealthGlobe

---
+++ Source/Aura/Private/UI/widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright notice for ObsMexGames .2025
+
+
+#include "UI/widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController = InWidgetController;
+	WidgetControllerSet();
+}
